be99ba3e9956f5184966058b5593b9e63993787e
Шеханов Евгений <neeptoon@mail.ru>
2019-09-22 08:23:51 +0300

    Добавляет методы получения случайных преимуществ и фотографий

--- js/main.js
+++ js/main.js
@@ -20,15 +20,25 @@ var avatarNumbers = getAvatarNumberList();
 var typeList = ['palace', 'flat', 'house', 'bungalo'];
 var checkinTime = ['12:00', '13:00', '14:00'];
 var checkoutTime = ['12:00', '13:00', '14:00'];
-var featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var photosList = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
-var getRandomThingsList = function (list) {
-  var randomList = [];
-  for (var i = 0; i < getRandomNumber(0, list.length - 1); i++) {
-    randomList[i] = list[i];
+var features = {
+  list: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  listRandom: function () {
+    var randomList = [];
+    for (var i = 0; i < getRandomNumber(0, features.list.length - 1); i++) {
+      randomList[i] = features.list[i];
+    }
+    return randomList;
   }
-  return randomList;
+};
+var photos = {
+  list: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+  listRandom: function () {
+    var randomList = [];
+    for (var i = 0; i < getRandomNumber(0, photos.list.length - 1); i++) {
+      randomList[i] = photos.list[i];
+    }
+    return randomList;
+  }
 };
 
 var getAdverts = function () {
@@ -43,8 +53,6 @@ var getAdverts = function () {
   var coordMaxX = 1024;
   var coordMinY = 130;
   var coordMaxY = 630;
-  var randomFeaturesList = getRandomThingsList(featuresList);
-  var randomPhotoList = getRandomThingsList(photosList);
 
   for (var i = 0; i < amountAdvert; i++) {
     var advertData = {
@@ -59,9 +67,9 @@ var getAdverts = function () {
         'guests': getRandomNumber(minGuests, maxGuests),
         'checkin': checkinTime[getRandomNumber(0, checkinTime.length - 1)],
         'checkout': checkoutTime[getRandomNumber(0, checkoutTime.length - 1)],
-        'features': randomFeaturesList,
+        'features': features.listRandom(),
         'description': 'строка с описанием',
-        'photos': randomPhotoList
+        'photos': photos.listRandom()
       },
       'location': {
         'x': getRandomNumber(coordMinX, coordMaxX),
@@ -75,6 +83,5 @@ var getAdverts = function () {
 };
 
 var adverts = getAdverts();
-console.log(adverts);
 
 
