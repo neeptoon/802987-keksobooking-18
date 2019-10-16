4dccaa637a835df88ff9f948b90bcf2dd2a0cf1e
Шеханов Евгений <neeptoon@mail.ru>
2019-10-16 08:03:10 +0300

    Изменяет алгоритм перемещения mainPin

--- js/moving-pins.js
+++ js/moving-pins.js
@@ -5,62 +5,47 @@
   var MAIN_PIN_HEIGHT = 72;
   var MAIN_PIN_WIDTH = 84;
 
-  var dragged = null;
+  // var dragged = null;
 
-  var shift = {
-    x: 0,
-    y: 0
-  };
-
-  var getAdvertAddress = function (evt) {
-    if (dragged) {
-      window.util.addressField.value = evt.currentTarget.offsetLeft + MAIN_PIN_WIDTH / 2 + ' ' + (evt.currentTarget.offsetTop + MAIN_PIN_HEIGHT);
-    }
-    window.util.addressField.value = (window.util.mainPin.offsetTop - shift.y) + MAIN_PIN_WIDTH / 2 + ' ' + ((window.util.mainPin.offsetLeft - shift.x) + MAIN_PIN_HEIGHT);
-  };
+  // var getAdvertAddress = function (evt) {
+  //   if (dragged) {
+  //     window.util.addressField.value = evt.currentTarget.offsetLeft + MAIN_PIN_WIDTH / 2 + ' ' + (evt.currentTarget.offsetTop + MAIN_PIN_HEIGHT);
+  //   }
+  //   window.util.addressField.value = (window.util.mainPin.offsetTop - shift.y) + MAIN_PIN_WIDTH / 2 + ' ' + ((window.util.mainPin.offsetLeft - shift.x) + MAIN_PIN_HEIGHT);
+  // };
 
   window.util.mainPin.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
-    dragged = false;
-
-    getAdvertAddress(evt);
-
     var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
+      x: evt.clientX - window.util.mainPin.getBoundingClientRect().left,
+      y: evt.clientY - window.util.mainPin.getBoundingClientRect().top
     };
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
-
-      dragged = true;
-
-      shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
+      var newCoords = {
+        x: moveEvt.clientX - startCoords.x - window.util.mapAdverts.getBoundingClientRect().left,
+        y: moveEvt.clientY - startCoords.y - window.util.mapAdverts.getBoundingClientRect().top
       };
 
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      if (startCoords.y < 130) {
-        startCoords.y = 130;
-      } else if (startCoords.y > 630) {
-        startCoords.y = 630;
+      if (newCoords.y < 130) {
+        newCoords.y = 130;
+      } else if (newCoords.y > 630) {
+        newCoords.y = 630;
+      } else if (newCoords.x < 0) {
+        newCoords.x = 0;
+      } else if (newCoords.x > window.util.mapAdverts.clientWidth - MAIN_PIN_WIDTH) {
+        newCoords.x = window.util.mapAdverts.clientWidth - MAIN_PIN_WIDTH;
       }
 
-      window.util.mainPin.style.top = (window.util.mainPin.offsetTop - shift.y) + 'px';
-      window.util.mainPin.style.left = (window.util.mainPin.offsetLeft - shift.x) + 'px';
-      getAdvertAddress(moveEvt);
 
+      window.util.mainPin.style.left = newCoords.x + 'px';
+      window.util.mainPin.style.top = newCoords.y + 'px';
     };
 
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
-
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
     };
