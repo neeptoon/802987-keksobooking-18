5bc908a3f1faf0ce94f8268857d1ba920176dfe5
Шеханов Евгений <neeptoon@mail.ru>
2019-10-17 06:36:39 +0300

    Исправляет вычисление координат метки

--- js/moving-pins.js
+++ js/moving-pins.js
@@ -2,24 +2,17 @@
 
 (function () {
 
-  var MAIN_PIN_HEIGHT = 72;
-  var MAIN_PIN_WIDTH = 84;
+  var MAIN_PIN_HEIGHT = 86;
+  var MAIN_PIN_WIDTH = 64;
 
-  var dragged = null;
-
-  var getAdvertAddress = function (evt) {
-    if (dragged) {
-      window.util.addressField.value = evt.currentTarget.offsetLeft + MAIN_PIN_WIDTH / 2 + ' ' + (evt.currentTarget.offsetTop + MAIN_PIN_HEIGHT);
-    }
+  var getAdvertAddress = function () {
     window.util.addressField.value = (window.util.mainPin.offsetLeft) + MAIN_PIN_WIDTH / 2 + ' ' + ((window.util.mainPin.offsetTop) + MAIN_PIN_HEIGHT);
   };
 
   window.util.mainPin.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
-    dragged = false;
-
-    getAdvertAddress(evt);
+    getAdvertAddress();
 
     var startCoords = {
       x: evt.clientX - window.util.mainPin.getBoundingClientRect().left,
@@ -28,9 +21,8 @@
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
-      dragged = true;
 
-      getAdvertAddress(moveEvt);
+      getAdvertAddress();
 
       var newCoords = {
         x: moveEvt.clientX - startCoords.x - window.util.mapAdverts.getBoundingClientRect().left,
@@ -39,8 +31,8 @@
 
       if (newCoords.y < window.util.MIN_Y - MAIN_PIN_HEIGHT) {
         newCoords.y = window.util.MIN_Y - MAIN_PIN_HEIGHT;
-      } else if (newCoords.y > window.util.MAX_Y) {
-        newCoords.y = window.util.MAX_Y;
+      } else if (newCoords.y > window.util.MAX_Y - MAIN_PIN_HEIGHT) {
+        newCoords.y = window.util.MAX_Y - MAIN_PIN_HEIGHT;
       } else if (newCoords.x < window.util.MIN_X) {
         newCoords.x = window.util.MIN_X;
       } else if (newCoords.x > window.util.mapAdverts.clientWidth - MAIN_PIN_WIDTH) {
