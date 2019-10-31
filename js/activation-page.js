42deec7caed8449bd06e56d669eaac6fdbaf90b1
Шеханов Евгений <neeptoon@mail.ru>
2019-10-31 07:48:57 +0300

    Добавляет функцию удаления меток объявлений

--- js/activation-page.js
+++ js/activation-page.js
@@ -16,8 +16,13 @@
     var errorElement = errorTemplate.cloneNode(true);
     var errorResetButton = errorElement.querySelector('.error__button');
     main.appendChild(errorElement);
-    errorResetButton.addEventListener('click', function () {
-      console.log('hello');
+    errorResetButton.addEventListener('click', returnStartPage);
+  };
+
+  var removePins = function () {
+    var pins = window.util.mapAdverts.querySelectorAll('button[type = "button"]');
+    pins.forEach(function (item) {
+      item.remove();
     });
   };
 
@@ -27,6 +32,7 @@
     window.util.mainPin.style.top = window.MainPinParams.START_COORDS_TOP + 'px';
     window.util.addressField.value = (window.util.mainPin.offsetLeft) + window.MainPinParams.MAIN_PIN_WIDTH / 2 + ' ' + ((window.util.mainPin.offsetTop) + window.MainPinParams.MAIN_PIN_HEIGHT);
     window.util.advertForm.reset();
+    removePins();
   };
 
   var advertFormSubmitHandler = function (evt) {
