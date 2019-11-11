e8236e2ef2fdae8a17effcb854ca5c3f0196a33a
Шеханов Евгений <neeptoon@mail.ru>
2019-11-11 06:59:31 +0300

    Добавляет фильтр по типу жилья

--- js/render-pins.js
+++ js/render-pins.js
@@ -1,11 +1,32 @@
 'use strict';
 (function () {
-  window.renderPins = function (adverts) {
-    window.advertPinsList = window.util.mapAdverts.querySelector('.map__pins');
-    var pinsFragment = document.createDocumentFragment();
-    adverts.forEach(function (item) {
-      pinsFragment.appendChild(window.getPin(item));
+  var AMOUNT_PINS = 4;
+  var adverts = [];
+  var pinsFragment = document.createDocumentFragment();
+  var housingTypeFilter = document.querySelector('#housing-type');
+
+  window.renderPins = function (data) {
+    adverts = data;
+    updateAdverts();
+  };
+
+  var updateAdverts = function () {
+    var sameHousingType = adverts.filter(function (it) {
+      return it.offer.type === housingTypeFilter.value;
     });
+
+    render(sameHousingType);
+  };
+
+  var render = function (data) {
+    window.advertPinsList = window.util.mapAdverts.querySelector('.map__pins');
+    data
+      .filter(function (it, i) {
+        return i <= AMOUNT_PINS;
+      })
+      .forEach(function (item) {
+        pinsFragment.appendChild(window.getPin(item));
+      });
     window.advertPinsList.appendChild(pinsFragment);
   };
 })();
